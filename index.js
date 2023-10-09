f5fa8c9a0f65c80d10b14a057c2f6a73b255eab9
Balaguru <balagurusiva2002@gmail.com>
2023-10-09 17:39:20 +0530

    modified:   index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
 dotenv.config();
-const app = express()
+const app = express() 
+mongoose.connect(process.env.MONGODBURL )
+    .then(() => {
+        console.log("db connected")
+        app.listen(process.env.PORT, () =>{console.log("listening in ",process.env.PORT)})
+    })
+    .catch((error) => {
+        console.log(error)  
+    })
 
-app.listen(process.env.PORT, () =>{console.log("listening in ",process.env.PORT)})
+ 
