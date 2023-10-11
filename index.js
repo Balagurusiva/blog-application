683d2dea783a6846a8176e52757e5c4903f77885
Balaguru <balagurusiva2002@gmail.com>
2023-10-11 13:49:13 +0530

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 
 dotenv.config();
+
 const app = express() 
-mongoose.connect(process.env.MONGODBURL )
+mongoose
+    .connect(process.env.MONGODBURL ) 
     .then(() => {
         console.log("db connected")
         app.listen(process.env.PORT, () =>{console.log("listening in ",process.env.PORT)})
